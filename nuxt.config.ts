ba82495cf08893e047af648ae6d9a8d4df71e546
Anonymous <username@users.noreply.github.com>
2026-03-14 21:14:32 +0100

    add base path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,6 +58,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
   app: {
+    baseURL: '/JennaPress/',
     head: {
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
