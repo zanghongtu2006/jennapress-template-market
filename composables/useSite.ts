301c2c4371d5b0234421e201c30c1dc3e109cf7a
Anonymous <username@users.noreply.github.com>
2026-03-15 10:14:12 +0100

    try to deploy

--- composables/useSite.ts
+++ composables/useSite.ts
@@ -2,13 +2,11 @@ import { joinURL } from 'ufo'
 import type { SiteConfig } from '~/types'
 
 export async function useSite() {
-  const baseURL = useRuntimeConfig().app.baseURL
+  const config = useRuntimeConfig()
+  const siteUrl = joinURL(config.app.baseURL, 'api/site')
 
   return await useAsyncData<SiteConfig>(
     'site-config',
-    () => $fetch(joinURL(baseURL, 'api/site')),
-    {
-      getCachedData: () => undefined
-    }
+    () => $fetch(siteUrl)
   )
 }
