3fa269bf220e87118bbed929bf931fc3ea8d2bb2
Anonymous <username@users.noreply.github.com>
2026-03-27 21:31:11 +0100

    update SEO, sitemap,searchengine

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -149,6 +149,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
+  modules: ['@nuxtjs/sitemap'],
+  sitemap: {
+    hostname: 'https://www.jennapress.com',
+  },
+
   hooks: {
     'pages:extend'(pages) {
       const root = process.cwd()
